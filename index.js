0a5a4ef5ea598d8fd3c91b9ada927dd8a86ba12b
Ivan Kozlov <103379782+RidiculousCircumstances@users.noreply.github.com>
2022-06-27 12:49:10 +0700

    switch for .http

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 import http from 'http';
 
+
 const host = '127.0.0.1';
 const port = 8000;
 
 const server = http.createServer((req, res) => {
+
+    switch (req.method) {
+        case 'GET':
+            switch (req.url) {
+                case '/one':
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'text/plain');
+                    res.end('Here we go again!');
+                    break;
+            }
+            break;
+    }
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello!');
